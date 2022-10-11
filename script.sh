edbdce68a5643307ba608cc2af188dae2a163a57
ykaiboussi <youssef_kaib@yahoo.com>
2022-10-10 22:23:17 -0500

    add -F so Prowler generates output folder with constant name instead

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running Prowler Scans"
-./prowler -b -n -f <REGION_TO_MODIFY> -g cislevel1 -M csv > prowler_report.csv
+./prowler -b -n -f <REGION_TO_MODIFY> -g cislevel1 -M csv -F
 echo "Converting Prowler Report from CSV to JSON"
 python converter.py
 echo "Loading JSON data into DynamoDB"
