a3265554df8a38ce3e6d75808f74c5bc3fb58fb9
ykaiboussi <youssef_kaib@yahoo.com>
2022-10-10 21:15:33 -0500

    update keys loading data into dynamodb

--- loader.py
+++ loader.py
@@ -11,21 +11,22 @@ dynamodb = boto3.resource('dynamodb', region_name=awsRegion)
 table = dynamodb.Table(prowlerDynamoDBTable)
 
 # CHANGE FILE AS NEEDED
-with open('format_prowler_report.json') as json_file:
+with open('output/format_prowler_report.json') as json_file:
     findings = json.load(json_file, parse_float = decimal.Decimal)
     for finding in findings:
         TITLE_ID = finding['TITLE_ID']
         TITLE_TEXT = finding['TITLE_TEXT']
-        RESULT = finding['RESULT']
-        NOTES = finding['NOTES']
+        RESULT = finding['CHECK_RESULT']
+        CHECK_RISK = finding['CHECK_RISK']
+        CHECK_SEVERITY = finding['CHECK_SEVERITY']
 
-        print("Adding finding:", TITLE_ID, TITLE_TEXT)
+        print("Adding finding:", TITLE_ID, TITLE_TEXT, CHECK_RISK,CHECK_SEVERITY)
 
         table.put_item(
            Item={
                'TITLE_ID': TITLE_ID,
                'TITLE_TEXT': TITLE_TEXT,
-               'RESULT': RESULT,
-               'NOTES': NOTES,
+               'CHECK_RISK': CHECK_RISK,
+               'CHECK_SEVERITY': CHECK_SEVERITY,
             }
         )
